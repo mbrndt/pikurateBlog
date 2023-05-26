610170c4b8f87feb7bd8e2366ccb722645e437c5
mbrndt <mareike.brandt@hotmail.de>
2023-05-26 18:23:02 +0900

    feat: expanded styling of homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,35 +1,36 @@
 import Link from "next/link";
-import landingpage from "./assets/landingpage.png";
+import landingpage from "./assets/landingpage.gif";
+import landingTitle from "./assets/landingTitle.jpg";
 import Image from "next/image";
 
 export default function HomePage() {
 	return (
-		<main className="flex flex-col items-center justify-between p-24 text-black font-serif">
-			<Image
-				src={landingpage}
-				alt="landing page"
-				width="fill"
-				className="z-0"
-			/>
-			<h1 className="text-3xl">
-				Welcome to <span className="text-blue-500">Pikurate!</span>
-			</h1>
-			<ul>
-				<li className="flex flex-col mt-10 gap-5 ">
-					<Link
-						className="text-2xl uppercase border rounded-xl border-gray-500 p-2 hover:bg-slate-50"
-						href="/blog"
-					>
-						Blog
-					</Link>
-					<Link
-						className="text-2xl uppercase border rounded-xl border-gray-500 p-2 hover:bg-slate-50"
-						href="/about"
-					>
-						About
-					</Link>
-				</li>
-			</ul>
+		<main className="flex flex-row items-center">
+			<div className="flex ml-20 flex-col">
+				<Image src={landingTitle} alt="" />
+				<h1 className="text-3xl mt-10">
+					Welcome to <span className="text-blue-500">Pikurate!</span>
+				</h1>
+				<ul>
+					<li className="flex flex-col mt-5 gap-5 ">
+						<Link
+							className="text-2xl uppercase border rounded-xl border-gray-500 p-2 hover:bg-slate-50"
+							href="/blog"
+						>
+							Blog
+						</Link>
+						<Link
+							className="text-2xl uppercase border rounded-xl border-gray-500 p-2 hover:bg-slate-50"
+							href="/about"
+						>
+							About
+						</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="">
+				<Image src={landingpage} alt="landing page" />
+			</div>
 		</main>
 	);
 }
