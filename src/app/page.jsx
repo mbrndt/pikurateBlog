0e8feb5fea1989ab909d6b6a76331ef916bdbbaf
mbrndt <mareike.brandt@hotmail.de>
2023-05-26 15:19:56 +0900

    feat: minor changes for better mobile UX

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,33 @@
 import Link from "next/link";
+import landingpage from "./assets/landingpage.png";
+import Image from "next/image";
 
 export default function HomePage() {
 	return (
 		<main className="flex flex-col items-center justify-between p-24 text-black font-serif">
-			<h1>Welcome to Pikurate</h1>
+			<Image
+				src={landingpage}
+				alt="landing page"
+				width="fill"
+				className="z-0"
+			/>
+			<h1 className="text-3xl">
+				Welcome to <span className="text-blue-500">Pikurate!</span>
+			</h1>
 			<ul>
-				<li className="flex flex-col">
-					<Link href="/">Home</Link>
-					<Link href="/blog">Blog</Link>
-					<Link href="/about">About</Link>
+				<li className="flex flex-col mt-10 gap-5 ">
+					<Link
+						className="text-2xl uppercase border rounded-xl border-gray-500 p-2 hover:bg-slate-50"
+						href="/blog"
+					>
+						Blog
+					</Link>
+					<Link
+						className="text-2xl uppercase border rounded-xl border-gray-500 p-2 hover:bg-slate-50"
+						href="/about"
+					>
+						About
+					</Link>
 				</li>
 			</ul>
 		</main>
