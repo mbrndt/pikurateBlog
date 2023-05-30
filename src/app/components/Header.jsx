04e678d61e28b958cc6cf44c6b22c92ecc6dff93
mbrndt <mareike.brandt@hotmail.de>
2023-05-30 14:02:48 +0900

    style: improved logo size

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -8,12 +8,8 @@ const Header = () => {
 		<header className="mt-2 border-b font-sans">
 			<div className="flex flex-row items-center justify-between">
 				<div className="text-blue-900 flex flex-row items-center gap-3 md:ml-5 font-sans">
-					<Link href="/">
-						<Image
-							src={logo}
-							className="w-1/2 h-1/2 md:w-25 md:h-25 mb-2"
-							alt="Pikurate Logo"
-						/>
+					<Link href="/" className="w-1/4 h-1/4">
+						<Image src={logo} className=" mb-2" alt="Pikurate Logo" />
 					</Link>
 				</div>
 				<div className="flex justify-end">
