042a76d52a1323107287ebbd027cab4f8a37691f
mbrndt <mareike.brandt@hotmail.de>
2023-06-07 09:46:34 +0900

    feat: added lazy loading to pictures

--- src/app/components/PictureWithLink.jsx
+++ src/app/components/PictureWithLink.jsx
@@ -6,7 +6,12 @@ function PictureWithLink({ image, link, title, description }) {
 	return (
 		<div className="flex flex-col items-center ">
 			<div className="flex items-center gap-5 border-b mt-5">
-				<Image src={image} alt="" className="rounded-lg w-1/4 h-1/4 mb-5" />
+				<Image
+					src={image}
+					alt=""
+					className="rounded-lg w-1/4 h-1/4 mb-5"
+					loading="lazy"
+				/>
 				<p className="mb-5">
 					<Link
 						href={link}
