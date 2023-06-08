f69aa3813c34a32b931ddd6fa78af2d00a85f34b
mbrndt <mareike.brandt@hotmail.de>
2023-06-08 15:23:46 +0900

    feat: small cleanup

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -1,4 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import * as admin from "firebase-admin";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyAmkan0aPbRo7Xi9O1tpePQDu_iS_WFjTs",
+	authDomain: "pikurate-blog.firebaseapp.com",
+	projectId: "pikurate-blog",
+	storageBucket: "pikurate-blog.appspot.com",
+	messagingSenderId: "141017044526",
+	appId: "1:141017044526:web:dbaed9dcaa0e552bb371bf",
+	measurementId: "G-Z3NK64B8N2",
+};
 
 if (!admin.apps.length) {
 	admin.initializeApp({
@@ -9,6 +26,10 @@ if (!admin.apps.length) {
 		}),
 	});
 }
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const db = admin.firestore();
 
