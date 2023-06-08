bab3b14706dfcdcc018914aef92c098e20c279e6
mbrndt <mareike.brandt@hotmail.de>
2023-06-08 10:26:32 +0900

    feat: started implementing firebase and added links in footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../assets/CI_color.png";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Footer = () => {
 	return (
@@ -14,20 +15,74 @@ export const Footer = () => {
 				</div>
 				<div className="flex flex-col gap-3">
 					<div className="text-blue-600 font-bold font-sans">About</div>
-					<div className="text-sm">About</div>
-					<div className="text-sm">Contact</div>
-					<div className="text-sm">Careers</div>
+					<Link
+						href="./about"
+						className="text-sm hover:text-base hover:text-blue-600"
+					>
+						About
+					</Link>
+					<Link
+						href="https://help.pikurate.com/hc/en-us/requests/new?_gl=1*4eywwv*_ga*MTQ1MDg2Mjg5MC4xNjgzNDQ1Nzgy*_ga_14JRT1ZRE5*MTY4NjE4NjQxNy4zNC4xLjE2ODYxODY0NTguMTkuMC4w"
+						rel="noopener"
+						target="_blank"
+						className="text-sm hover:text-base hover:text-indigo-500"
+					>
+						Contact
+					</Link>
+					<Link
+						href="https://www.wanted.co.kr/company/14534"
+						rel="noopener"
+						target="_blank"
+						className="text-sm hover:text-base hover:text-violet-500"
+					>
+						Careers
+					</Link>
 				</div>
 				<div className="flex flex-col gap-3 ">
 					<div className="text-blue-600 font-bold font-sans">Legal</div>
-					<div className="text-sm">Privacy Policy</div>
-					<div className="text-sm">Terms of Service</div>
+					<Link
+						href="https://www.pikurate.com/privacy"
+						rel="noopener"
+						target="_blank"
+						className="text-sm hover:text-base hover:text-blue-600"
+					>
+						Privacy Policy
+					</Link>
+					<Link
+						href="https://www.pikurate.com/agreement"
+						rel="noopener"
+						target="_blank"
+						className="text-sm hover:text-base hover:text-indigo-500"
+					>
+						Terms of Service
+					</Link>
 				</div>
 				<div className="flex flex-col gap-3">
 					<div className="text-blue-600 font-bold font-sans">Social</div>
-					<div className="text-sm">Facebook</div>
-					<div className="text-sm">Twitter</div>
-					<div className="text-sm">Instagram</div>
+					<Link
+						href="https://www.facebook.com/pikurate/"
+						rel="noopener"
+						target="_blank"
+						className="text-sm hover:text-base hover:text-blue-600"
+					>
+						Facebook
+					</Link>
+					<Link
+						href="https://twitter.com/pikurate"
+						rel="noopener"
+						target="_blank"
+						className="text-sm hover:text-base hover:text-indigo-500"
+					>
+						Twitter
+					</Link>
+					<Link
+						href="https://www.instagram.com/pikurate/?hl=en"
+						rel="noopener"
+						target="_blank"
+						className="text-sm hover:text-base hover:text-violet-500"
+					>
+						Instagram
+					</Link>
 				</div>
 			</div>
 		</div>
