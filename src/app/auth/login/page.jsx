8f1abe4b7fb08c73f0c447778410fff30c8380b3
mbrndt <mareike.brandt@hotmail.de>
2023-06-14 09:53:20 +0900

    feat: login complete

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,28 +1,34 @@
+"use client";
+
 import { FcGoogle } from "react-icons/fc";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "src/app/firebase.js";
+import { useRouter } from "next/navigation";
+
 export default function Login() {
+	const router = useRouter();
 	//Sign in with google
 	const googleProvider = new GoogleAuthProvider();
-	const googleSignIn = async () => {
+	const GoogleLogin = async () => {
 		try {
 			const result = await signInWithPopup(auth, googleProvider);
-			const credential = GoogleAuthProvider.credentialFromResult(result);
-			const token = credential.accessToken;
-			const user = result.user;
-			console.log(user);
+			console.log(result.user);
+			router.push("/");
 		} catch (error) {
 			console.log(error);
 		}
 	};
 
 	return (
-		<div className="shadow-xl mt-32 p-10 text-gray-7">
-			<h1 className="text-2xl font-medium">Join Today</h1>
+		<div className="shadow-xl mx-5 mt-32 md:mt-5 md:mb-20 p-10 text-gray-700 rounded-lg md:flex md:flex-col md:items-center ">
+			<h2 className="text-2xl font-sans font-medium">Join Today</h2>
 			<div className="py-4">
 				<h3 className="py-4">Sign in with one of the providers</h3>
-				<button className="text-white bg-blue-500 w-full font-medium rounded-lg flex align-middle p-4">
-					<FcGoogle className="mr-2" size={30} />
+				<button
+					onClick={GoogleLogin}
+					className="text-gray-700 bg-blue-300 font-sans w-fit font-medium rounded-lg flex align-middle p-4 gap-2"
+				>
+					<FcGoogle className="text-2xl" />
 					Sign in with Google
 				</button>
 			</div>
--- src/app/firebase.js
+++ src/app/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import * as admin from "firebase-admin";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.FIREBASE_PROJECT_ID,
 	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
@@ -19,18 +19,7 @@ const firebaseConfig = {
 	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
-// if (!admin.apps.length) {
-// 	admin.initializeApp({
-// 		credential: admin.credential.cert({
-// 			projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-// 			clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-// 			privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-// 		}),
-// 	});
-// }
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
